6963eb24b8c45facd89efc3ffc3a17db2008b580
venugopal5501 <venugopal14082002@gmail.com>
2024-09-02 18:40:18 +0530

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,48 @@
 <template>
   <h1>{{ msg }}</h1>
-  <h2>Home Component</h2>
+  <h2>Home Component length of digital is {{ "Digital".length }}</h2>
+  <h3>Email : {{ email }}</h3>
+  <h3>Mobile : {{ mobile }}</h3>
+  <h3>Address : {{ "Chennai" }}</h3>
+
 </template>
 
 <script>
 
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      email: 'venugopal@gmail.com',
+      mobile: '123456789',
+    }
+  },
+  methods:{
+    getAddress(Address) {
+        return Address
+      }
+
+  },
   props: {
     msg: String
   }
-}
+
+    
+
+    }
+  
+  // getata(){
+  //   return {
+  //   hai:'hi',
+  //   hello : 'hell'
+  //   }
+  // },
+  
+
 </script>
 
-<style>
-h2,h1{
+<style scoped>
+h2 {
   color: red;
 }
 </style>
